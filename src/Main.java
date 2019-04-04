7183f733eeb40dbcf87c50768df68eb6f0dfd442
2102062 <2102062@geneva304.org>
2019-04-04 09:02:11 -0500

    Graph works now

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,21 @@ import java.util.Arrays;
 public class Main {
     public final static double kSampleRate = 0.01;
     public static void main(String[] args){
-        ArrayList<Double> xpos = new ArrayList<>();
-        ArrayList<Double> ypos = new ArrayList<>();
+        ArrayList<Double> xpos,ypos,xangle,yangle;
+        xpos = new ArrayList<>();
+        ypos = new ArrayList<>();
+        xangle = new ArrayList<>();
+        yangle = new ArrayList<>();
         QuinticSpline spline = new QuinticSpline(0,0,10,20,90,45);
         for (double i = 0; i<1; i+=kSampleRate){
             xpos.add(spline.getY(i));
             ypos.add(spline.getX(i));
+            xangle.add(i);
+            yangle.add(spline.getAngle(i));
             Debug.print(Double.toString(spline.getVelocity(i)),1);
         }
 
         PathChart chart = new PathChart(xpos, ypos);
+        PathChart chart2 = new PathChart(xangle, yangle);
     }
 }
