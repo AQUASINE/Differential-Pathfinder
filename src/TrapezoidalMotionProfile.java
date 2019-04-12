ce9cc7a0a738f1cdb466b82a1277be20e1cd1f9c
Link3141 <39888768+Link3141@users.noreply.github.com>
2019-04-12 08:30:35 -0500

    work on ytsprziubvsd

--- src/TrapezoidalMotionProfile.java
+++ src/TrapezoidalMotionProfile.java
@@ -27,7 +27,7 @@ public class TrapezoidalMotionProfile {
         this.max_acceleration = max_acceleration;
         }
     }
-    public class State{
+    public static class State{
         @SuppressWarnings("MemberName")
         public double position;
         @SuppressWarnings("MemberName")
@@ -44,7 +44,8 @@ public class TrapezoidalMotionProfile {
         }
     }
 
-    public TrapezoidalMotionProfile (State goal, State initial, Constraints constraints){
+
+    public TrapezoidalMotionProfile (State initial, State goal, Constraints constraints){
         m_direction = shouldFlipAccel(initial, goal, constraints) ? 1 : -1;
         m_constraints = constraints;
         m_initial = direction(initial);
@@ -70,6 +71,102 @@ public class TrapezoidalMotionProfile {
         m_endDecel = m_endFullSpeed + accelerationTime - cutoffEnd;
     }
 
+    public TrapezoidalMotionProfile(State goal, Constraints constraints) {
+        this(new State(0,0), goal, constraints);
+    }
+
+    @SuppressWarnings("ParameterName")
+    public boolean isFinished(double t) {
+        return t >= totalTime();
+    }
+    public double totalTime() {
+        return m_endDecel;
+    }
+    public double timeLeftUntil(double target) {
+        double position = m_initial.position * m_direction;
+        double velocity = m_initial.velocity * m_direction;
+
+        double endAccel = m_endAccel * m_direction;
+        double endFullSpeed = m_endFullSpeed * m_direction - endAccel;
+
+        if (target < position) {
+            endAccel = -endAccel;
+            endFullSpeed = -endFullSpeed;
+            velocity = -velocity;
+        }
+
+        endAccel = Math.max(endAccel, 0);
+        endFullSpeed = Math.max(endFullSpeed, 0);
+        double endDeccel = m_endDecel - endAccel - endFullSpeed;
+        endDeccel = Math.max(endDeccel, 0);
+
+        double acceleration = m_constraints.max_acceleration;
+        double decceleration = -m_constraints.max_acceleration;
+
+        double distToTarget = Math.abs(target - position);
+
+        if (distToTarget < 1e-6) {
+            return 0;
+        }
+
+        double accelDist = velocity * endAccel + 0.5 * acceleration * endAccel * endAccel;
+
+        double deccelVelocity;
+        if (endAccel > 0) {
+            deccelVelocity = Math.sqrt(Math.abs(velocity * velocity + 2 * acceleration * accelDist));
+        } else {
+            deccelVelocity = velocity;
+        }
+
+        double deccelDist = deccelVelocity * endDeccel + 0.5 * decceleration * endDeccel * endDeccel;
+
+        deccelDist = Math.max(deccelDist, 0);
+
+        double fullSpeedDist = m_constraints.max_velocity * endFullSpeed;
+
+        if (accelDist > distToTarget) {
+            accelDist = distToTarget;
+            fullSpeedDist = 0;
+            deccelDist = 0;
+        } else if (accelDist + fullSpeedDist > distToTarget) {
+            fullSpeedDist = distToTarget - accelDist;
+            deccelDist = 0;
+        } else {
+            deccelDist = distToTarget - fullSpeedDist - accelDist;
+        }
+
+        double accelTime = (-velocity + Math.sqrt(Math.abs(velocity * velocity + 2 * acceleration
+                * accelDist))) / acceleration;
+
+        double deccelTime = (-deccelVelocity + Math.sqrt(Math.abs(deccelVelocity * deccelVelocity
+                + 2 * decceleration * deccelDist))) / decceleration;
+
+        double fullSpeedTime = fullSpeedDist / m_constraints.max_velocity;
+
+        return accelTime + fullSpeedTime + deccelTime;
+    }
+    @SuppressWarnings("ParameterName")
+    public State calculate(double t) {
+        State result = m_initial;
+
+        if (t < m_endAccel) {
+            result.velocity += t * m_constraints.max_acceleration;
+            result.position += (m_initial.velocity + t * m_constraints.max_acceleration / 2.0) * t;
+        } else if (t < m_endFullSpeed) {
+            result.velocity = m_constraints.max_velocity;
+            result.position += (m_initial.velocity + m_endAccel * m_constraints.max_acceleration
+                    / 2.0) * m_endAccel + m_constraints.max_velocity * (t - m_endAccel);
+        } else if (t <= m_endDecel) {
+            result.velocity = m_goal.velocity + (m_endDecel - t) * m_constraints.max_acceleration;
+            double timeLeft = m_endDecel - t;
+            result.position = m_goal.position - (m_goal.velocity + timeLeft
+                    * m_constraints.max_acceleration / 2.0) * timeLeft;
+        } else {
+            result = m_goal;
+        }
+
+        return direction(result);
+    }
     private boolean shouldFlipAccel(State initial, State goal, Constraints constraints){
         double velocityChange = goal.velocity - initial.velocity;
         double positionChange = goal.position - initial.position;
