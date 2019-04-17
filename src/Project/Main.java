a2e158dbddb321ddefa53a9e886159ea66b84dd9
2102062 <2102062@geneva304.org>
2019-04-17 07:57:32 -0500

    LIST WORKS!!!!

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -6,6 +6,7 @@ import com.github.sh0nk.matplotlib4j.PythonExecutionException;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
@@ -22,16 +23,23 @@ public class Main {
         ArrayList<Double> m_dataY = new ArrayList<>();
         ArrayList<Double> m_dataV = new ArrayList<>();
         ArrayList<Double> m_dataAngle = new ArrayList<>();
+        ArrayList<Double[]> m_data = new ArrayList<>();
         Plot plt = Plot.create();
         double t = 0;
+        int x = 0;
         while(!profile.isFinished(t)){
             TrapezoidalMotionProfile.State curr_state = profile.calculate(t);
             m_dataX.add(path.getX(curr_state.position/path.total_length));
             m_dataY.add(path.getY(curr_state.position/path.total_length));
             m_dataV.add(curr_state.velocity);
             m_dataAngle.add(path.getAngle(curr_state.position/path.total_length));
+            m_data.add(new Double[]{m_dataX.get(x), m_dataY.get(x), m_dataV.get(x), m_dataAngle.get(x)});
             t += Constants.kSampleRate;
+            x++;
             Debug.print(profile.calculate(t).position*Math.cos(Math.toRadians(path.getAngleFromSplines(t))),0);
+        }
+        for(int g = 0; g < m_data.size(); g++) {
+            Debug.print(Arrays.toString(m_data.get(g)), 2);
         }
         plt.plot()
                 .add(m_dataX,m_dataY);
